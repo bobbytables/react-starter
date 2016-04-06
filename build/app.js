17415501149c8d66e8b2f7ae3f5a5fa1f7fd9b8b
BobbyTables <robert@creativequeries.com>
2016-04-06 18:11:37 -0400

    Initial Commit.

---
+++ build/app.js
@@ -0,0 +1,6 @@
+// main.js
+ReactDOM.render(React.createElement(
+  'h1',
+  null,
+  'Sup Dawg!'
+), document.getElementById('example'));
---
+++ src/app.js
@@ -0,0 +1,5 @@
+// main.js
+ReactDOM.render(
+  <h1>Sup Dawg!</h1>,
+  document.getElementById('example')
+);
